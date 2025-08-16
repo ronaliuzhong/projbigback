fe82e7be4ec29267b5dde46543374e5aba75d016
ronaliuzhong <liuzhong.r@northeastern.edu>
2025-08-15 23:40:12 -0400

    debug

--- script.js
+++ script.js
@@ -2,18 +2,33 @@
 const APIKEY = "b1b502c2ced044b6992662f0533692c0"
 const SEARCHAPI = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + APIKEY + "&query="
 const BASICAPI = "https://api.spoonacular.com/recipes/random?apiKey=" + APIKEY
+const TESTAPI = SEARCHAPI + "pizza";
+
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
-returnRecipes(BASICAPI);
+returnRecipes(TESTAPI);
 
 function returnRecipes(url) {
     fetch(url).then(res => res.json())
         .then(function(data) {
-            console.log(data.results);
-            data.results.forEach(element => {
+
+            console.log(data);
+
+            const recipes = data.results || data.recipes;
+
+            if (recipes.length === 0) {
+                main.innerHTML = '<h1>No results found</h1>';
+                return;
+            }
+
+            console.log(recipes);
+
+            recipes.forEach(element => {
+            
+                
             const div_card = document.createElement('div');
             div_card.setAttribute('class', 'card');
 
@@ -33,7 +48,7 @@ function returnRecipes(url) {
             const center = document.createElement('center');
 
             title.innerHTML = `${element.title}`;
-            image.src = IMG_PATH + element.poster_path;
+            image.src = element.image;
 
             center.appendChild(image);
             div_card.appendChild(center);
